89ce8e9ccb3870b761f4b69e1ca602d93a94da9a
sagi <sagi21805@gmail.com>
2024-11-06 18:21:05 +0200

    Added more protocols

--- src/protocol_utils.rs
+++ src/protocol_utils.rs
@@ -5,6 +5,10 @@ use pnet::packet::ipv4::{Ipv4, Ipv4Packet};
 use pnet::packet::ipv6::{Ipv6, Ipv6Packet};
 use pnet::packet::tcp::{Tcp, TcpPacket};
 use pnet::packet::udp::{Udp, UdpPacket};
+use pnet::packet::dns::{Dns, DnsPacket};
+use pnet::packet::icmp::{Icmp, IcmpPacket};
+use pnet::packet::icmpv6::{Icmpv6, Icmpv6Packet};
+// use pnet::packet::dhcp::{Dhcp, DhcpPacket};
 use pnet::packet::Packet;
 
 macro_rules! into_header {
@@ -22,7 +26,7 @@ macro_rules! into_header {
                         $(
                             $field_name: self.[<get_ $field_name>](),
                         )*
-                        payload: vec![0],
+                        payload: vec![],
                     }
                 }
 
@@ -47,3 +51,6 @@ into_header! { Ipv6, version, traffic_class, flow_label, payload_length, next_he
 into_header! { Tcp, source, destination, sequence, acknowledgement, data_offset, reserved, flags, window, checksum, urgent_ptr, options }
 into_header! { Udp, source, destination, length, checksum }
 into_header! { Arp, hardware_type, protocol_type, hw_addr_len, operation, proto_addr_len, sender_hw_addr, sender_proto_addr, target_hw_addr, target_proto_addr}
+into_header! { Dns, id, is_response, opcode, additional, additional_rr_count, authorities, authority_rr_count, is_answer_authenticated, is_authoriative, is_non_authenticated_data, is_recursion_available, is_recursion_desirable, is_truncated, queries, query_count, rcode, responses, response_count, zero_reserved }
+into_header! { Icmp, icmp_type, icmp_code, checksum }
+into_header! { Icmpv6, icmpv6_type, icmpv6_code, checksum }
