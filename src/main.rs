802605daaee84b2ac60c96bc17e08286ec545cf6
sagi <sagi21805@gmail.com>
2024-11-09 21:43:53 +0000

    tested and added dependencies

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ mod packet_stream;
 mod protocol_utils;
 mod sniffer;
 mod utils;
+mod sender;
+mod packet_handler;
+
+use std::net::Ipv4Addr;
+
+use packet_handler::PacketHandler;
+use pnet::ipnetwork::Ipv4Network;
 use sniffer::Sniffer;
 
 fn main() {
@@ -14,20 +21,27 @@ fn main() {
         100
     );
 
-    loop {
-        let t = std::time::Instant::now();
-        match sniffer.sniff() {
-            Ok(packet) => {
-                println!("Packet: {}", packet)
-            }
-            Err(e) => {
-                eprintln!("Err: {}", e)
-            }
-        }
-        println!("Sniff time: {}ns", t.elapsed().as_nanos());
-        let s = sniffer.stream();
-        println!("Stream: {}", s.len())
-    }
+    let handler = PacketHandler::new(interface_name, 2048);
+
+    let task = handler.arp_scan(Ipv4Network::new(
+        Ipv4Addr::new(10, 100, 102, 7), 
+        24
+    ).expect("Can't create network")
+    );
+    // loop {
+    //     let t = std::time::Instant::now();
+    //     match sniffer.sniff() {
+    //         Ok(packet) => {
+    //             println!("Packet: {}", packet)
+    //         }
+    //         Err(e) => {
+    //             eprintln!("Err: {}", e)
+    //         }
+    //     }
+    //     println!("Sniff time: {}ns", t.elapsed().as_nanos());
+    //     let s = sniffer.stream();
+    //     println!("Stream: {}", s.len())
+    // }
 
 
     
